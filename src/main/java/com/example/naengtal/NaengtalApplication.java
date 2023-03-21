78d0bf7cee87ab5b3df616ad6646e5c5eb34171d
Seo Dabin <tjekqls7@naver.com>
2023-03-22 03:02:42 +0900

    Initial commit

---
+++ src/main/java/com/example/naengtal/NaengtalApplication.java
@@ -0,0 +1,13 @@
+package com.example.naengtal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaengtalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaengtalApplication.class, args);
+	}
+
+}
