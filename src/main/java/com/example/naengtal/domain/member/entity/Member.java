3134e53d41047af932f491d5aaea8c3d0a040197
Seo Dabin <tjekqls7@naver.com>
2023-04-02 01:19:17 +0900

    feat: "member entity class 생성"
    - fridge_id는 추후에 추가할 예정

---
+++ src/main/java/com/example/naengtal/domain/member/entity/Member.java
@@ -0,0 +1,35 @@
+package com.example.naengtal.domain.member.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "member")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Member {
+    @Id
+    @Column(name = "id", nullable = false)
+    String id;
+
+    @Column(name = "name", nullable = false)
+    String name;
+
+    @Column(name = "password", nullable = false)
+    String password;
+
+    @Builder
+    public Member(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+}
