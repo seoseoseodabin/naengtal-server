207dfdc4a2550c7dbb411c920988b262829a237c
jihong <clara5091@naver.com>
2023-04-03 01:59:29 +0900

    feat: "UserDetails, service 생성"
    스프링 시큐리티에서 사용자 정보 담을 JwtUserDetails(UserDetails 상속), JwtUserDetailsService 구현

---
+++ src/main/java/com/example/naengtal/global/auth/jwt/JwtUserDetails.java
@@ -0,0 +1,63 @@
+package com.example.naengtal.global.auth.jwt;
+
+import com.example.naengtal.domain.member.entity.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JwtUserDetails implements UserDetails {
+
+    private final Member member;
+    private final List<String> roles = new ArrayList<>();
+
+    public JwtUserDetails(Member member) {
+        this.member = member;
+        roles.add("ROLE_USER");
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getId();
+    }
+
+    // 계정 만료 여부
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // 계정 잠김 여부
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 비밀번호 만료 여부
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 사용자 활성화 여부
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
