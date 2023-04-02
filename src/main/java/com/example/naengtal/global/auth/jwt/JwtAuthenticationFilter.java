7e613c51589cb67743e8956312a2f3afd2892411
jihong <clara5091@naver.com>
2023-04-03 02:00:27 +0900

    feat: "JwtToken 생성, 검증 구현"
    토큰 생성:90일간 유효한 accessToken 생성하여 리턴
    토큰 검증:key로 서명 복호화 후 유효기간 확인, 사용자 정보 확인

---
+++ src/main/java/com/example/naengtal/global/auth/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.example.naengtal.global.auth.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    // JWT 토큰의 인증 정보(Authentication 객체)를 SecurityContext 에 저장
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = resolveToken(request);
+
+        if (StringUtils.hasText(token)
+                && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest httpServletRequest) {
+        String token = httpServletRequest.getHeader(AUTHORIZATION_HEADER);
+        if (token != null && token.startsWith(BEARER_PREFIX)) {
+            return token.substring(BEARER_PREFIX.length());
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/example/naengtal/global/auth/jwt/JwtTokenProvider.java
@@ -0,0 +1,105 @@
+package com.example.naengtal.global.auth.jwt;
+
+import com.example.naengtal.global.auth.dto.TokenDto;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+    @Value("${jwt.validate-in-seconds}")
+    private long accessTokenValidateTime;
+    private static final String AUTHORITIES_KEY = "auth";
+    private Key key;
+
+    @PostConstruct
+    private void init() {
+        key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        accessTokenValidateTime *= 1000;
+    }
+
+    public TokenDto generateToken(Authentication authentication) {
+        long now = (new Date()).getTime();
+
+        String token = Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authentication.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.joining(",")))
+                .setExpiration(new Date(now + accessTokenValidateTime))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+        return new TokenDto(token, now + accessTokenValidateTime);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            parseClaims(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.error("Invalid JWT token");
+        } catch (ExpiredJwtException e) {
+            log.error("expired token");
+        } catch (UnsupportedJwtException e) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims string is empty.");
+        }
+        return false;
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = parseClaims(token);
+        List<GrantedAuthority> authorities = parseAuthorities(claims);
+
+        validateAuthority(authorities);
+
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    private void validateAuthority(List<GrantedAuthority> authorities) {
+        for (GrantedAuthority authority : authorities) {
+            if (authority.getAuthority().equals("ROLE_USER")) {
+                return;
+            }
+        }
+        throw new RuntimeException("Invalid Authority");
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private List<GrantedAuthority> parseAuthorities(Claims claims) {
+        return Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(""))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
