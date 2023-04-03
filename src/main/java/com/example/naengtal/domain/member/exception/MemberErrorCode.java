172feb073fbb18495ed269ca40d83106236b1012
Seo Dabin <tjekqls7@naver.com>
2023-04-04 00:58:40 +0900

    refactor: "구현한 에러 처리 핸들러로 에러 처리 수정"
    - MemberErrorCode 클래스 생성

---
+++ src/main/java/com/example/naengtal/domain/member/exception/MemberErrorCode.java
@@ -0,0 +1,19 @@
+package com.example.naengtal.domain.member.exception;
+
+import com.example.naengtal.global.error.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum MemberErrorCode implements ErrorCode {
+
+    UNAVAILABLE_ID(HttpStatus.BAD_REQUEST, "Unavailable id"),
+    WRONG_CONFIRM_PASSWORD(HttpStatus.BAD_REQUEST, "Wrong confirm password"),
+    ;
+
+
+    private HttpStatus httpStatus;
+    private String message;
+}
--- src/main/java/com/example/naengtal/domain/member/service/AccountService.java
+++ src/main/java/com/example/naengtal/domain/member/service/AccountService.java
@@ -5,11 +5,15 @@ import com.example.naengtal.domain.fridge.repository.FridgeRepository;
 import com.example.naengtal.domain.member.dao.MemberRepository;
 import com.example.naengtal.domain.member.dto.SignUpRequestDto;
 import com.example.naengtal.domain.member.entity.Member;
+import com.example.naengtal.global.error.RestApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+
+import static com.example.naengtal.domain.member.exception.MemberErrorCode.UNAVAILABLE_ID;
+import static com.example.naengtal.domain.member.exception.MemberErrorCode.WRONG_CONFIRM_PASSWORD;
 
 @Service
 @Transactional
@@ -24,13 +28,13 @@ public class AccountService {
 
     public void saveMember(SignUpRequestDto signUpRequestDto) {
         // id 중복 검사
-        memberRepository.findById(signUpRequestDto.getId()).ifPresent(a ->
-            System.out.println("id already exist")
-        );
+        memberRepository.findById(signUpRequestDto.getId()).ifPresent(a -> {
+            throw new RestApiException(UNAVAILABLE_ID);
+        });
 
         // 비밀번호와 비밀번호 확인 일치하는지 검사
         if (!signUpRequestDto.getPassword().equals(signUpRequestDto.getConfirmPassword()))
-            System.out.println("wrong confirm password");
+            throw new RestApiException(WRONG_CONFIRM_PASSWORD);
 
         // 냉장고 생성
         Fridge fridge = new Fridge();
