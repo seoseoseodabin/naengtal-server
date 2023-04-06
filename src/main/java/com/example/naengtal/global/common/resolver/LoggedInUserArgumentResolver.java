af5b9a446311fea0c295cf072448f2ac544a0784
Seo Dabin <tjekqls7@naver.com>
2023-04-07 00:11:26 +0900

    feat: "argument resolver 구현"

---
+++ src/main/java/com/example/naengtal/global/common/resolver/LoggedInUserArgumentResolver.java
@@ -0,0 +1,44 @@
+package com.example.naengtal.global.common.resolver;
+
+import com.example.naengtal.domain.member.dao.MemberRepository;
+import com.example.naengtal.domain.member.entity.Member;
+import com.example.naengtal.global.common.annotation.LoggedInUser;
+import com.example.naengtal.global.error.RestApiException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import static com.example.naengtal.domain.member.exception.MemberErrorCode.MEMBER_NOT_FOUND;
+
+@Component
+@RequiredArgsConstructor
+public class LoggedInUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        // 호출되는 컨트롤러의 파리미터 어노테이션과 타입 검사
+        return parameter.getParameterAnnotation(LoggedInUser.class) != null
+                && parameter.getParameterType().equals(Member.class);
+    }
+
+    // supportsParameter 콜백 함수에서 true를 반환했을 경우 실행
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory) throws Exception {
+
+        String id = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new RestApiException(MEMBER_NOT_FOUND));
+    }
+}
