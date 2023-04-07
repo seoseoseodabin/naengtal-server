8e60075353054f53d737057bc9014a72f2b09ded
chjih <81521128+chjih@users.noreply.github.com>
2023-04-07 10:20:40 +0000

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,11 +9,9 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+source ~/.bash_profile
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
