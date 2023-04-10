dcf0172d35c35d99b7c6a87c6b5daf8cf06b57f7
Seo Dabin <tjekqls7@naver.com>
2023-04-10 10:16:07 +0900

    refactor : "접근지정자 수정"

--- src/main/java/com/example/naengtal/domain/member/dto/SignUpRequestDto.java
+++ src/main/java/com/example/naengtal/domain/member/dto/SignUpRequestDto.java
@@ -1,26 +1,28 @@
 package com.example.naengtal.domain.member.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SignUpRequestDto {
 
     @NotBlank(message = "NAME_IS_MANDATORY")
     @Size(min = 1, max = 10)
-    String name;
+    private String name;
 
     @NotBlank(message = "ID_IS_MANDATORY")
     @Size(min = 1, max = 15)
-    String id;
+    private String id;
 
     @NotBlank(message = "PASSWORD_IS_MANDATORY")
-    String password;
+    private String password;
 
     @NotBlank(message = "CONFIRM_PASSWORD_IS_MANDATORY")
-    String confirmPassword;
+    private String confirmPassword;
 }
