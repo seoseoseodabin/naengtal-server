5a58e40f5f0f646d4b1e7e4f02edd539469bcd43
Seo Dabin <tjekqls7@naver.com>
2023-04-12 00:37:56 +0900

    feat : "회원탈퇴 구현"
    - 추후 냉장고 공유 기능 구현 후 수정 필요

--- src/main/java/com/example/naengtal/domain/member/controller/AccountApiController.java
+++ src/main/java/com/example/naengtal/domain/member/controller/AccountApiController.java
@@ -2,15 +2,14 @@ package com.example.naengtal.domain.member.controller;
 
 import com.example.naengtal.domain.member.dto.SignUpRequestDto;
 import com.example.naengtal.domain.member.dto.SignUpResponseDto;
+import com.example.naengtal.domain.member.entity.Member;
 import com.example.naengtal.domain.member.service.AccountService;
+import com.example.naengtal.global.common.annotation.LoggedInUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("account/")
@@ -24,5 +23,12 @@ public class AccountApiController {
         SignUpResponseDto dto = accountService.saveMember(signUpRequestDto);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(dto);
+    }
+
+    @DeleteMapping("delete")
+    public ResponseEntity<String> withdrawal(@LoggedInUser Member member) {
+        accountService.deleteMember(member);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("success");
     }
 }
