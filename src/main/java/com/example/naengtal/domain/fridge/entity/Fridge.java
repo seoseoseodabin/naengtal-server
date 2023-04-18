a9f058eda3322d64bef255037b2a7f869f3720aa
Seo Dabin <tjekqls7@naver.com>
2023-04-18 16:12:39 +0900

    feat : "Fridge와 Member 엔티티 클래스 사이에 양방향 연관관계 매핑"

--- src/main/java/com/example/naengtal/domain/fridge/entity/Fridge.java
+++ src/main/java/com/example/naengtal/domain/fridge/entity/Fridge.java
@@ -1,11 +1,13 @@
 package com.example.naengtal.domain.fridge.entity;
 
+import com.example.naengtal.domain.member.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "Fridge")
@@ -19,4 +21,7 @@ public class Fridge {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "fridge_id", nullable = false)
     int id;
+
+    @OneToMany(mappedBy = "fridge")
+    List<Member> sharedMembers;
 }
