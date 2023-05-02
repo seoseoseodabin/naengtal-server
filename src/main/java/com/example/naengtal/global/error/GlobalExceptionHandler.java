6dad18993627a8f31a60ac692f08f0a14de1dbf8
Seo Dabin <tjekqls7@naver.com>
2023-05-02 15:53:21 +0900

    refactor : "RestApiException, MethodArgumentNotValidException 제외한 Exception 발생 시 INTERNAL_ERROR_CODE로 응답"
    - 주석처리 풂

--- src/main/java/com/example/naengtal/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/naengtal/global/error/GlobalExceptionHandler.java
@@ -20,12 +20,12 @@ public class GlobalExceptionHandler {
         return handleExceptionInternal(errorCode);
     }
 
-//    패키지 구조 같은 거 숨기는 목적. 근데 개발 단계에서는 오류잡을 때 불편하므로 주석처리 해 놓았음
-//    @ExceptionHandler({Exception.class})
-//    public ResponseEntity<Object> handleAllException(Exception e) {
-//        ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
-//        return handleExceptionInternal(errorCode);
-//    }
+    // 패키지 구조 같은 거 숨기는 목적. 근데 개발 단계에서는 오류잡을 때 불편하므로 주석처리 해 놓았음
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<ErrorResponseDto> handleAllException(Exception e) {
+        ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
+        return handleExceptionInternal(errorCode);
+    }
 
     private ResponseEntity<ErrorResponseDto> handleExceptionInternal(ErrorCode errorCode) {
         return ResponseEntity.status(errorCode.getHttpStatus())
