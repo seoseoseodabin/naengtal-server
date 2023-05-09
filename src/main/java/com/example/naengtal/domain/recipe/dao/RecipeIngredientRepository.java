5f37fb699d842d87aed0e02a515fa2e14e510dc5
Seo Dabin <tjekqls7@naver.com>
2023-05-10 03:54:37 +0900

    feat : "레시피 코드로 ingredient, process 찾아올 수 있게 함"

--- src/main/java/com/example/naengtal/domain/recipe/dao/RecipeIngredientRepository.java
+++ src/main/java/com/example/naengtal/domain/recipe/dao/RecipeIngredientRepository.java
@@ -4,6 +4,10 @@ import com.example.naengtal.domain.recipe.entity.RecipeIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Integer> {
+
+    List<RecipeIngredient> findByRecipeCodeOrderByIngredientNumber(int recipeCode);
 }
--- src/main/java/com/example/naengtal/domain/recipe/dao/RecipeProcessRepository.java
+++ src/main/java/com/example/naengtal/domain/recipe/dao/RecipeProcessRepository.java
@@ -4,6 +4,10 @@ import com.example.naengtal.domain.recipe.entity.RecipeProcess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeProcessRepository extends JpaRepository<RecipeProcess, Integer> {
+
+    List<RecipeProcess> findByRecipeCodeOrderByProcessNumber(int recipeCode);
 }
